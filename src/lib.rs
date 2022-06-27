f59badf6b158b3ed6fb5593acf519f8c2b534f8f
egor-vaskon <egor.vaskonyan@gmail.com>
2022-06-27 14:36:17 +0300

    doc: add crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+//! Rust standard library has [`format_args!`] macro that lets
+//! you combine format string and its arguments into the [`Arguments`] structure
+//! without allocations in cost of directly referencing parts of format string,
+//! so the result must be used immediately (to satisfy borrow checker).
+//!
+//! [`format_args!`]: https://doc.rust-lang.org/std/macro.format_args.html
+//! [`Arguments`]: https://doc.rust-lang.org/stable/std/fmt/struct.Arguments.html
+//!
+//! This crates offers `pformat_args!` macro that returns `impl Display` instance
+//! that can be used just like any normal structure holding provided format arguments.
+//!
+//! <br/>
+//!
+//! # Example
+//!
+//! The usage is almost the same as  [`format_args!`] except that all `{}`
+//! placeholders must be empty.
+//!
+//! ```
+//! use pformat_macro::pformat_args;
+//!
+//! let result_str = pformat_args!("1 + 1 = {}", 3);
+//! println!("{}", result_str) //prints 1 + 1 = 3
+//! ```
+
 use crate::generator::expand;
 use syn::{parse_macro_input, Expr};
 
